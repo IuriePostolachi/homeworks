5146c1b3acb508c2bd58320c2afa430423e3d638
Iurie Postolachi <47760455+IuriePostolachi@users.noreply.github.com>
2019-03-11 20:10:09 +0200

    ДЗ №22

---
+++ lesson22/js/forms.js
@@ -0,0 +1,83 @@
+$(document).ready(function () {
+
+  $(".modal__form").validate({
+    rules: {
+      username: "required",
+      username: {
+        required: true,
+        minlength: 2,
+        maxlength: 15
+      },
+      email: {
+        required: true,
+        email: true
+      }
+    },
+    messages: {
+      username: {
+        required: "Пожалуйста введите ваше имя",
+        minlength: jQuery.validator.format(" Длина от {0} букв"),
+        maxlength: jQuery.validator.format(" Длина до {0} букв")
+      },
+      email: {
+        required: "Нам нужен ваш емайл для отправки информаций",
+        email: "Введите емайл правильно"
+      }
+    }
+  });
+  $(".brif-form").validate({
+    rules: {
+      username: "required",
+      username: {
+        required: true,
+        minlength: 2,
+        maxlength: 15
+      },
+      email: {
+        required: true,
+        email: true
+      }
+    },
+    messages: {
+      username: {
+        required: "Пожалуйста введите ваше имя",
+        minlength: jQuery.validator.format(" Длина от {0} букв"),
+        maxlength: jQuery.validator.format(" Длина до {0} букв")
+      },
+      email: {
+        required: "Нам нужен ваш емайл для отправки информаций",
+        email: "Введите емайл правильно"
+      }
+    }
+  });
+  $(".offer__form").validate({
+    rules: {
+      username: "required",
+      username: {
+        required: true,
+        minlength: 2,
+        maxlength: 15
+      },
+      email: {
+        required: true,
+        email: true
+      }
+    },
+    messages: {
+      username: {
+        required: "Пожалуйста введите ваше имя",
+        minlength: jQuery.validator.format(" Длина от {0} букв"),
+        maxlength: jQuery.validator.format(" Длина до {0} букв")
+      },
+      email: {
+        required: "Нам нужен ваш емайл для отправки информаций",
+        email: "Введите емайл правильно"
+      }
+    }
+  });
+});
+
+$(document).ready(function () {
+  $('.phone').mask('+7 (999) 999-99-99')
+
+});
