27f8dca2dfe1294531c570cbc6379683bbedf486
Iurie Postolachi <47760455+IuriePostolachi@users.noreply.github.com>
2019-03-12 13:39:13 +0200

    Домашнее задание №23

---
+++ lesson23/js/modal.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+    // переменная кнопки NavBar-a "Перезвоните мне"
+    var button = $('#button');
+    // переменная модального окна активируемой кнопкой "Перезвоните мне"
+    var modal = $('#modal');
+    // переменная кнопки "закрыть модальное окно"
+    var close = $('#close');
+    // переменная кнопки "отправить" модального окна
+    var send = $('#send');
+
+    // открытие модального окна
+    button.on('click', function() {
+        modal.addClass('modal_active');
+    });
+
+    // закрытие модального окна при нажатий на кнопку 'закрыть'
+    close.on('click', function() {
+        modal.removeClass('modal_active');
+    });
+});
